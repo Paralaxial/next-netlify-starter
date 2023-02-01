bce93edea7f3e7a24fea30077145a62c157f5461
Paralaxial <123896389+Paralaxial@users.noreply.github.com>
2023-02-01 00:40:22 +0000

    change text

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          I'm learning React.
         </p>
       </main>
 
