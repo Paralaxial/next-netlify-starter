4f9f276903b45e3925c55b89b16db9347e3f127c
Paralaxial <123896389+Paralaxial@users.noreply.github.com>
2023-02-01 01:59:07 +0000

    tailwind

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,24 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div>
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome!" />
-        <p className="description">
-          I'm learning React.
-        </p>
+        <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+          <p className="text-3xl text-gray-700 font-bold mb-5">
+            Welcome!
+          </p>
+          <p className="text-gray-500 text-lg">
+            React and Tailwind CSS in action
+          </p>
+        </div>
       </main>
 
-      <Footer />
+      {/*<Footer />*/}
     </div>
   )
 }
